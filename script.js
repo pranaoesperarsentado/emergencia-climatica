341364d9d16cccd7746edea270f841136132c3a2
pranaoesperarsentado <pranaoesperarsentado@gmail.com>
2026-05-23 20:09:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+function gerarEmail() {
 
+const cidade =
+document.getElementById("cidade").value;
+
+const estado =
+document.getElementById("estado").value;
+
+const assunto =
+encodeURIComponent(
+"Solicitação sobre preparação climática"
+);
+
+const texto =
+encodeURIComponent(
+`Prezados(as),
+
+Solicito informações sobre as medidas de preparação e resposta a possíveis emergências climáticas em ${cidade} - ${estado}  relacionadas ao fenômeno denominado "Super El Niño", previsto para o segundo semestre de 2026.
+
+Gostaria de saber:
+
+- existência de plano de contingência;
+- sistemas de alerta;
+- ações preventivas;
+- informações públicas disponíveis.
+
+Atenciosamente`
+);
+
+const email =
+"contato@prefeitura.gov.br";
+
+window.location.href =
+`mailto:${email}?subject=${assunto}&body=${texto}`;
+
+}
